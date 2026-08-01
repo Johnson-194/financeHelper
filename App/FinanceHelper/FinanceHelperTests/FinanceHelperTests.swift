66ef6d105005abeb069327eb341fd3a90666ca69
Johnson <chenjohnson961@gmail.com>
2026-08-01 13:58:14 +0100

    FEAT: Created project on XCode

---
+++ App/FinanceHelper/FinanceHelperTests/FinanceHelperTests.swift
@@ -0,0 +1,19 @@
+//
+//  FinanceHelperTests.swift
+//  FinanceHelperTests
+//
+//  Created by Johnson Chen on 01/08/2026.
+//
+
+import Testing
+@testable import FinanceHelper
+
+struct FinanceHelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
