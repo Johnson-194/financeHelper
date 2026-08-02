d48547770e5de80754e6500170d6972a7eb2434d
Johnson <chenjohnson961@gmail.com>
2026-08-02 13:51:16 +0100

    FEAT: Removed some default code. Made prototype main page, including title and buttons (no functionality yet)

--- App/FinanceHelper/FinanceHelper/ContentView.swift
+++ App/FinanceHelper/FinanceHelper/ContentView.swift
@@ -1,55 +1,20 @@
-//
-//  ContentView.swift
-//  FinanceHelper
-//
-//  Created by Johnson Chen on 01/08/2026.
-//
-
 import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        VStack {
+            Text("Finance Tracker")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            HStack {
+                Button("Edit Data") {
+                    
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                Button("View Summary") {
+                    
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
             }
         }
     }
@@ -57,5 +22,4 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
--- App/FinanceHelper/FinanceHelper/Item.swift
+++ App/FinanceHelper/FinanceHelper/Item.swift
@@ -1,10 +1,3 @@
-//
-//  Item.swift
-//  FinanceHelper
-//
-//  Created by Johnson Chen on 01/08/2026.
-//
-
 import Foundation
 import SwiftData
 
