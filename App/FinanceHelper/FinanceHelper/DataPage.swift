87b4203ea2d6aebfd1219c97cb1b35e9bfbaa276
Johnson <chenjohnson961@gmail.com>
2026-08-07 15:29:10 +0100

    FEAT: Added functionality and validation checks for the Amount Field in the Data Page.

--- App/FinanceHelper/FinanceHelper/DataPage.swift
+++ App/FinanceHelper/FinanceHelper/DataPage.swift
@@ -5,35 +5,53 @@ struct DataPage: View {
     @State private var amount = ""
     @State private var selectedDate = Date() // Date() sets it to the current date
     
-    @State private var category: String? = nil
+    @State private var category: String?
     private var categories = ["Food", "Groceries", "Entertainment", "Clothing", "Transportation", "Savings", "Other"]
+    
+    // Keyboard:
+    @FocusState private var isFocused: Bool // Initially false due to property wrapper
     
     
     var body: some View {
-        VStack {
-            Text("Add Data")
-                .font(.largeTitle)
-                .padding()
-            
-            DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
-                .frame(width: 300)
-            
-            TextField("Amount", text: $amount)
-                .frame(width: 300)
-            
-            Picker("Category", selection: $category) {
-                Text("Select Category").tag(nil as String?)
-                ForEach(categories.sorted(), id: \.self) { category in
-                    Text(category).tag(category as String?)
+        ZStack {
+            Color(.white)
+                .onTapGesture {
+                    isFocused = false
+                }
+            VStack {
+                Text("Add Data")
+                    .font(.largeTitle)
+                    .padding()
+                
+                DatePicker("Select Date", selection: $selectedDate, displayedComponents: .date)
+                    .frame(width: 300)
+                
+                TextField("Amount", text: $amount)
+                    .frame(width: 300)
+                    .keyboardType(.decimalPad) // So phone users can only see a number pad with decimal
+                    .focused($isFocused) // So keyboard can disappear
+                    .onChange(of: amount) { old, new in
+                            if new.filter({ $0 == "." }).count > 1 {
+                                amount = old
+                            } else if new.contains(where: { !$0.isNumber && $0 != "." }) {
+                                amount = old
+                            }
+                        } // Validation check: We only allow empty field or valid decimal numbers
+                
+                Picker("Category", selection: $category) {
+                    Text("Select Category").tag(nil as String?)
+                    ForEach(categories.sorted(), id: \.self) { category in
+                        Text(category).tag(category as String?)
+                    }
+                }
+                .pickerStyle(.menu)
+                
+                Button("Add Transaction") {
+                    
                 }
             }
-            .pickerStyle(.menu)
-            
-            Button("Add Transaction") {
-                
-            }
+            .padding()
         }
-        .padding()
     }
 }
 
